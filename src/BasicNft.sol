e9117649222c406265048fc76b86b1808fcd4086
Shubham Nikam <nikamshubham224@gmail.com>
2025-09-16 20:58:55 +0530

    Added uri for ipfs

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -8,11 +8,28 @@ contract BasicNft is ERC721{
 
     uint256 private s_tokenCounter;
 
+    /*
+balanceOf(owner) → how many NFTs a person owns.
+
+ownerOf(tokenId) → who owns a specific NFT.
+
+transferFrom(from, to, tokenId) → transfer NFT.
+
+safeTransferFrom(...) → transfer NFT safely to contracts.
+
+approve(to, tokenId) → give someone permission to transfer your NFT.
+
+setApprovalForAll(operator, approved) → approve an operator for all your NFTs.
+
+    */
+
     constructor()ERC721("Dogie","Dog"){
         s_tokenCounter = 0;
     }
 
-    function mintNft()public{};  
+     function mintNft (){
+        returns "ipfs://_________random_ipfs_id"
+     }
     
     function tokenURI(uint256 tokenId) public view override returns (string memory){
         return 
