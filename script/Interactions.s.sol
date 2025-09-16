7211468ed256d0b4cbc40ffd6ae3c55b1134fb64
Shubham Nikam <nikamshubham224@gmail.com>
2025-09-17 03:53:07 +0530

    Basic Nft script

---
+++ script/Interactions.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        // read deployed contract address from .env
+        address nftAddress = vm.envAddress("BASIC_NFT_ADDRESS");
+        mintNftOnContract(nftAddress);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
